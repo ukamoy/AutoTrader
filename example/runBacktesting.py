dd40928582883740794f48fe5ef9590cab07920e
ukamoy <rinzia@msn.com>
2019-02-18 17:19:03 +0800

    [MOD] make setting section

--- example/runBacktesting.py
+++ example/runBacktesting.py
@@ -1,39 +1,34 @@
 # encoding: UTF-8
-"""
-展示如何执行策略回测。
-"""
-from __future__ import division
+"""展示如何执行策略回测"""
+"""setting start here"""
+engine_settings = {
+            "setStartDate":('20180615 00:00',0), # 设置回测用的数据起始日期，initHours 默认值为 0
+            "setEndDate" : ('20190115 23:59',),
+            "setCapital":1000000,      # 设置起始资金，默认值是1,000,000
+            "setSlippage" : (0.002,),  # 股指1跳
+            "setRate" : (5/10000,),    # 万0.3
+            "setSize":(300,),          # 股指合约大小 
+            "setPriceTick":(0.2,),     # 股指最小价格变动
+            "setDatabase" : ("VnTrader_1Min_Db",),
+            "setLog":(True,"D:\\vnpy_data\\log\\",), # 设置是否输出日志和交割单, 默认不输出, 默认路径在当前文件夹
+            "setCachePath":("D:\\vnpy_data\\",)      # 设置本地数据缓存的路径，默认存在用户文件夹内
+            }
+srategy_settings = {'symbolList':['tBTCUSD:bitfinex']}
+from StrategyBollBand import BollBandsStrategy as Strategy
+"""setting end here"""
+
 from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine
-from vnpy.trader.app.ctaStrategy.ctaBase import *
 
 if __name__ == '__main__':
-    from StrategyBollBand import BollBandsStrategy
-    
-    # 创建回测引擎
+    # 创建回测引擎，模式为K线
     engine = BacktestingEngine()
-
-    # 设置引擎的回测模式为K线
     engine.setBacktestingMode(engine.BAR_MODE)
-    # 设置使用的历史数据库
-    engine.setDatabase(MINUTE_DB_NAME)
-
-    # 设置回测用的数据起始日期，initHours 默认值为 0
-    engine.setStartDate('20180731 06:00',initHours=1)   
-    engine.setEndDate('20180801 08:00')
-    # 设置产品相关参数
-    engine.setCapital(1000000)  # 设置起始资金，默认值是1,000,000
-    engine.setSlippage(0.2)     # 股指1跳
-    engine.setRate(0.3/10000)   # 万0.3
-    engine.setSize(300)         # 股指合约大小 
-    engine.setPriceTick(0.2)    # 股指最小价格变动
-    
-    # 策略报告默认不输出，默认文件夹生成于当前文件夹下
-    engine.setLog(True,"D:\\vnpy_data\\log\\")        # 设置是否输出日志和交割单, 默认值是不输出False
-    engine.setCachePath("D:\\vnpy_data\\") # 设置本地数据缓存的路径，默认存在用户文件夹内
+    for attr, setting in engine_settings.items():
+        func = engine.__getattribute__(attr)
+        func(*setting)
     
     # 在引擎中创建策略对象
-    d = {'symbolList':['tBTCUSD:bitfinex']}
-    engine.initStrategy(BollBandsStrategy, d)
+    engine.initStrategy(Strategy, srategy_settings)
     
     # 开始跑回测
     engine.runBacktesting()
--- example/runOptimization.py
+++ example/runOptimization.py
@@ -1,53 +1,98 @@
 # encoding: UTF-8
+"""展示如何执行策略参数优化"""
+"""setting start here"""
+engine_settings = {
+            "setStartDate":('20180615 00:00',0),
+            "setEndDate" : ('20190115 23:59',),
+            "setSlippage" : (0.002,),
+            "setRate" : (5/10000,),
+            "setDatabase" : ("VnTrader_1Min_Db",),
+            }
+# optimization_setting
+opt_target = 'sharpeRatio' #优化目标
+opt_settings = {'symbolList':['eos.usd.q:okef'], 'barPeriod':300} # 策略常量
+task_lists = [
+    {"task_name":"task1_BB_Window", 
+        "pick_opt_param":[                  # pick_opt_param 用于找到最优的参数
+            {"param_name":'fastWindow', "range":(10, 50, 5)},
+            {"param_name":'slowWindow', "range":(50, 100, 5)}
+        ]},                              
 
-"""
-展示如何执行参数优化。
-"""
+    {"task_name":"task2_MaPeriod", 
+        "pick_opt_param":[
+            {"param_name":'kdjMaPeriod', "range":(50, 100, 5)},
+            {"param_name":'signalMaPeriod', "range":(10, 30, 1)}
+                    ]},       
+    
+    {"task_name":"task3_lowVolRate", 
+        "pick_best_param":[                 # pick_best_param 用于找到参数组结果的最大值
+            {"param_name":'lowVolRate', "range":(1, 20, 1)}
+                    ]}
+    ]
+from StrategyBollBand import BollBandsStrategy as Strategy
+"""setting end here"""
 
-from __future__ import division
-from __future__ import print_function
+from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine,OptimizationSetting
+from datetime import datetime
+import pandas as pd
+import os
+import json
 
-from vnpy.trader.app.ctaStrategy.ctaBacktesting import BacktestingEngine, OptimizationSetting
-from vnpy.trader.app.ctaStrategy.ctaBase import *
+def start_optimize_n_output(task, setting, param_mode):
+    for param in task[param_mode]:
+        setting.addParameter(param["param_name"], *param["range"])
+    opt_output = engine.runParallelOptimization(Strategy, setting)
+    result_list = []
+    col=[]
+    for result in opt_output:
+        param,target,stat = result
+        param = eval(param)
+        result_dict = {**param, **stat}
+        result_list.append(result_dict)
+    col.extend(param.keys())
+    col.extend(['sharpeRatio','endBalance','totalReturn','maxDdPercent','totalDays','totalTradeCount'])
+    result_df = pd.DataFrame(result_list, columns=col)
+    result_df.sort_values(opt_target, ascending = False, inplace = True)
+    result_df.to_csv(f"{now}_opt_result_{task['task_name']}.csv")
+    func = str_func_pair[param_mode]
+    for param in task[param_mode]:
+        func(result_df, param["param_name"], setting)
+
+def pick_opt_param(df, param_name, setting):
+    count = 0
+    for i in [20,10,5]:
+        opt=df.head(i)
+        count += opt[param_name].value_counts()
+    setting.paramDict[param_name] = [float(count.idxmax())]
+
+def pick_best_param(df, param_name, setting):
+    best_param = list(df[param_name])[0]
+    setting.paramDict[param_name] = [best_param]
+
+def run_tasks(setting):
+    for task in task_lists:
+        task_name, param_mode = task
+        start_optimize_n_output(task, setting, param_mode)
+    with open(f'opt_result_{now}.json', "w", encoding="utf-8") as f:
+        json.dump(setting.paramDict, f,indent=4)
+
+    print("*** tasks finished ****")
 
 if __name__ == '__main__':
-    from StrategyBollBand import BollBandsStrategy    
-
-    # 创建回测引擎
+    # 创建回测引擎，模式为K线
     engine = BacktestingEngine()
-    
-    # 设置引擎的回测模式为K线
     engine.setBacktestingMode(engine.BAR_MODE)
+    for attr, setting in engine_settings.items():
+        func = engine.__getattribute__(attr)
+        func(*setting)
 
-    # 设置回测用的数据起始日期
-    engine.setStartDate('20170616 09:00',initHours=1)               # 设置回测用的数据起始日期
-    engine.setEndDate('20170702 00:00')
-    # 设置产品相关参数
-    engine.setSlippage(0.2)     # 股指1跳
-    engine.setRate(0.3/10000)   # 万0.3
-    engine.setSize(300)         # 股指合约大小 
-    engine.setPriceTick(0.2)    # 股指最小价格变动
-    
-    # 设置使用的历史数据库
-    engine.setDatabase(MINUTE_DB_NAME)
+    str_func_pair = {"pick_opt_param": pick_opt_param,
+                 'pick_best_param': pick_best_param}
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
     
     # 跑优化
-    setting = OptimizationSetting()                 # 新建一个优化任务设置对象
-    setting.setOptimizeTarget('capital')            # 设置优化排序的目标是策略净盈利
-    setting.addParameter('lmaPeriod', 12, 20, 2)    # 增加第一个优化参数atrLength，起始12，结束20，步进2
-    setting.addParameter('cciPeriod', 20, 30, 5)        # 增加第二个优化参数atrMa，起始20，结束30，步进5
-    setting.addParameter('rsiEntry', 5)            # 增加一个固定数值的参数
-    setting.addParameter('symbolList',['tBTCUSD:bitfinex','tBTCUSD:bitfinex'])
-    
-    # 性能测试环境：I7-3770，主频3.4G, 8核心，内存16G，Windows 7 专业版
-    # 测试时还跑着一堆其他的程序，性能仅供参考
-    import time    
-    start = time.time()
-    
-    # 运行单进程优化函数，自动输出结果，耗时：359秒
-    # engine.runOptimization(BollBandsStrategy, setting)            
-    
-    # 多进程优化，耗时：89秒
-    engine.runParallelOptimization(BollBandsStrategy, setting)
-    
-    print(u'耗时：%s' %(time.time()-start))
+    setting = OptimizationSetting()
+    setting.setOptimizeTarget(opt_target)
+    for opt_name, opt_setting in opt_settings.items():
+        setting.addParameter(opt_name, opt_setting)
+    run_tasks(setting)
